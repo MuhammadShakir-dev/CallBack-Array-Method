39bc6fe37ae2968a605545668a0b6f4a4ee81d4a
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-27 23:32:43 +0500

    Create implicetReturn.js

---
+++ implicetReturn.js
@@ -0,0 +1,12 @@
+// Implicet Return is the more shoter way and smater way to make functions expressions in javascript.
+// NOTE:
+// Implicet Return works only when there is only one value that you want to return.
+
+const leapyear = (year) => (
+          year % 4 == 0
+    )
+    
+const add = (a,b) => (a+b);
+
+const add = (a,b) => a+b;
+
