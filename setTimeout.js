5805bc901045af07c096fb0fe5844a77040a5e38
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-28 00:03:42 +0500

    Update setTimeout.js

--- setTimeout.js
+++ setTimeout.js
@@ -1,1 +1,6 @@
+// set timeout gives you a return after some specific time that you have benn set to it.
+// 4000 = 4sec
 
+setTimeout(() => {
+    return console.log("Are your still there........?")
+},4000);
