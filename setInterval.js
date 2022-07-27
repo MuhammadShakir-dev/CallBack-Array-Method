008c5ee4d2147ef661a3bbd07d3b514bd67fde57
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-28 00:17:41 +0500

    Update setInterval.js

--- setInterval.js
+++ setInterval.js
@@ -1,1 +1,9 @@
+// Set interval has almost the same work as setTime out but its continuesly printing the return after some specific time that you have been set to it.
+// for stoping setinterval you have to use clearInterval();
 
+const id = setInterval(()=> {
+    return Math.random();
+},2000);
+
+// for stopping it
+// clearInterval(id).
