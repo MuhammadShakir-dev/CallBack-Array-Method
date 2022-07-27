f4ac6b9dd9b53c5f2635b2abb88369156fdcf934
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-27 17:48:53 +0500

    Create ArrowFunctions.js

---
+++ ArrowFunctions.js
@@ -0,0 +1,9 @@
+// Arrow Function is the new way of defining function experissions  in javascript
+
+const square = (x) => {
+    return x**2;
+}
+
+const rollDice = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
