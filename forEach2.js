b8cd975ce5712085546229bf2d28ccce159b5c1e
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-27 15:11:37 +0500

    Update forEach2.js

--- forEach2.js
+++ forEach2.js
@@ -1,1 +1,17 @@
 
+  <script>
+        let  ary = [
+    
+   { name: "Avenger end game",
+    score : 91},
+    
+    {name : "Avenger infinity war",
+    score : 93}
+    
+    ];
+    
+    ary.forEach(function(movies){
+        document.write(movies.name + " " + "    " + movies.score + "/100" + "<br>");
+    }
+    );
+    </script>
