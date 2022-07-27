7d18d660d03a280fe69ec3d79ab22724e58218b7
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-27 14:54:15 +0500

    Update forEachMethod().js

--- forEachMethod().js
+++ forEachMethod().js
@@ -1,1 +1,22 @@
 
+/*
+
+For Each Array Method.
+
+It allows you to run some code once per item in some array. for each are used by progmers by a log tim but a new version of for each is 
+for of.
+
+
+*/
+
+const ary = [2,4,6,8,10,12,14,16,18,20];
+
+ary.forEach(function(el){
+     if(el % 2 == 0){
+         document.write(el)
+         }  
+});
+
+
+
+
