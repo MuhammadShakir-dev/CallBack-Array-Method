8fab00c75ba0a762f221af42d6e573547ac4af05
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-29 00:02:23 +0500

    Update this.js

--- this.js
+++ this.js
@@ -1,1 +1,23 @@
+// this keyword behaves different in traditional functions and different in an arrow function .
 
+const studentData = {
+    
+    fname : " Muhammad ",
+    lname : "Shakir",
+    Age  :  21,
+    University : "SMIU",
+    stdID : "BSE-20F-077",
+    dept : "Software Engineering (Morning)",
+    
+    fullName : function(){
+        return `${this.fname}${this.lname}`;
+    },
+    
+    Education : function(){
+        return `${this.University} - ${this.stdID} `
+    },  
+};
+
+console.log(studentData.fullName());
+console.log(studentData.Education());
+console.log(studentData.dept);
