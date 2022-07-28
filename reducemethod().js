372dc8235a083f4409f31d2be640a980138dd717
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-28 23:31:24 +0500

    Update reducemethod().js

--- reducemethod().js
+++ reducemethod().js
@@ -1,2 +1,36 @@
 // Reduce execute a reducer function on each element of the array resulting in a single value.
 
+const prices = [19.99,250,399,799,1299];
+
+const totalSum = prices.reduce((total,price) => {
+    return total + price;
+});
+
+console.log(totalSum);
+
+//Lets take another exapmle.
+
+// finding the min and max price from this array of proces.
+
+const minPrice = prices.reduce((min,max) => {
+     if(min < max){
+         return min
+     }else{
+         return max
+     }
+} );
+
+console.log(minPrice);
+
+
+// lets do it for finding maximum value from the array.
+
+const maxPrice = prices.reduce((min,max) => {
+    if(max > min){
+        return max
+    }else{
+        return min
+    }
+});
+
+console.log(maxPrice);
