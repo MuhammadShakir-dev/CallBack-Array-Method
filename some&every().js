1ada719bbbc0e62a1577bd281066a5132862383e
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-28 15:58:04 +0500

    Update some&every().js

--- some&every().js
+++ some&every().js
@@ -1,1 +1,6 @@
+// SOme and every returns true and false values according to the test.
+// if any one element to the array passes the test then some returns a true  value otherwise it will give you a false value
+// if all element of the aray passes the test then every gives you a true value other wise it will give you a false value.
+
+
 
