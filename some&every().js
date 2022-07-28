8ab39b9afbd1d089cf728feec1306fd4202ecf3c
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-28 16:04:07 +0500

    Update some&every().js

--- some&every().js
+++ some&every().js
@@ -3,4 +3,13 @@
 // if all element of the aray passes the test then every gives you a true value other wise it will give you a false value.
 
 
+const ary = [01,10,23,45,63,43,65];
 
+let test = ary.every( x => x < 70 );
+console.log(test);
+
+
+const ary = [01,10,23,45,63,43,65,84];
+
+let test = ary.some( x => x > 70 );
+console.log(test);
